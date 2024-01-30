69c8ee3a114677552c19485d04100ff45e2da16d
sagara11 <thiensangkdtt@gmail.com>
2024-01-30 18:44:41 +0700

    first commit

---
+++ lab.py
@@ -0,0 +1,13 @@
+import sys
+
+# Path to a directory containing multiple modules
+# module_dir = "/home/thong/python_lab/my_labs"
+# sys.path.append(module_dir)
+
+
+# Now you can import any module within that directory
+import lab_1
+import lab_2
+
+lab_1.fib(10)
+print(lab_2.fib2(100))
