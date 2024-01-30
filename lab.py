a1052f75547ef7b9e4d3f8cf97dfca7c0fc79ebf
Tommy Nguyen <thiensangkdtt@gmail.com>
2024-01-31 00:11:08 +0700

    update 31/01/2024

--- lab.py
+++ lab.py
@@ -1,13 +1,14 @@
 import sys
-
+from my_labs import fib
+from my_labs import fib2
 # Path to a directory containing multiple modules
 # module_dir = "/home/thong/python_lab/my_labs"
 # sys.path.append(module_dir)
-
+# from my_labs import *
 
 # Now you can import any module within that directory
-import lab_1
-import lab_2
+# import lab_1
+# import lab_2
 
-lab_1.fib(10)
-print(lab_2.fib2(100))
+fib(10)
+print(fib2(100))
---
+++ my_labs/__init__.py
@@ -0,0 +1,8 @@
+from lab_1 import fib
+from lab_2 import fib2
+print("My package is being imported!")
+# __all__ = ["lab_1", "hello"]
+
+
+def hello():
+    print("hello world")
