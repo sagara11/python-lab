bb7ef4d9d0eed5eea1c37085687081587bf35069
Tommy Nguyen <thiensangkdtt@gmail.com>
2024-02-03 23:09:42 +0700

    learn generator expression, exec

--- classes_learn.py
+++ classes_learn.py
@@ -179,3 +179,37 @@ my_list = [1, 2, 3]
 
 print(instance.change_value.__self__)
 print(instance.change_value.__func__)
+
+
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+
+rev = Reverse('spam')
+print(iter(rev))
+for char in rev:
+    print(char)
+
+
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+
+print(reverse("gold").__dir__)
+
+for char in reverse('golf'):
+    print(char)
